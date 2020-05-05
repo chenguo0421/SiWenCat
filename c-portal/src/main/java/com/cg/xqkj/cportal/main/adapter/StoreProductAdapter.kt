1a2762acd132aa88cba435cd33b69f01a8a63313
chenguo0421 <wushengyuan1hao@163.com>
2020-05-05 14:52:42 +0800

    【add】商城首页新增相关内容

--- c-portal/src/main/java/com/cg/xqkj/cportal/main/adapter/StoreProductAdapter.kt
+++ c-portal/src/main/java/com/cg/xqkj/cportal/main/adapter/StoreProductAdapter.kt
@@ -8,11 +8,15 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import cn.com.cg.ccommon.utils.DeviceUtils
+import cn.com.cg.ccommon.utils.ToastUtils
+import cn.com.cg.router.manager.RouterManager
+import com.bumptech.glide.Glide
 import com.cg.xqkj.cportal.R
 import com.cg.xqkj.cportal.main.bean.StoreProductsBean
 import com.pdog.dimension.dp
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.portal_item_store_product_newcomme.view.*
+import kotlinx.android.synthetic.main.portal_item_store_product_recommendtoday.view.*
 
 class StoreProductAdapter(var context: Context, var data:ArrayList<StoreProductsBean>) : RecyclerView.Adapter<StoreProductAdapter.MyHolder>(){
 
@@ -86,6 +90,79 @@ class StoreProductAdapter(var context: Context, var data:ArrayList<StoreProducts
     }
 
     private fun setRecommendTodayItem(holder: StoreProductAdapter.HolderForRecommendToday, position: Int) {
+        holder.itemView.lv_title.text = data[position].title?.get(0)?.title
+        holder.itemView.lv_title.setTextColor(Color.parseColor(data[position].title?.get(0)?.titleColor))
+
+        holder.itemView.lv_title1.text = data[position].title?.get(1)?.title
+        holder.itemView.lv_title1.setTextColor(Color.parseColor(data[position].title?.get(1)?.titleColor))
+
+        holder.itemView.rtc_hot.setContentText(data[position].title?.get(0)?.hotText)
+        holder.itemView.rtc_hot.setContentTextColor(Color.parseColor(data[position].title?.get(0)?.hotTextColor))
+        holder.itemView.rtc_hot.setContentBackgroundColor(Color.parseColor(data[position].title?.get(0)?.hotTextBGColor))
+        holder.itemView.rtc_hot.invalidate()
+
+        holder.itemView.rtc_hot1.setContentText(data[position].title?.get(1)?.hotText)
+        holder.itemView.rtc_hot1.setContentTextColor(Color.parseColor(data[position].title?.get(1)?.hotTextColor))
+        holder.itemView.rtc_hot1.setContentBackgroundColor(Color.parseColor(data[position].title?.get(1)?.hotTextBGColor))
+        holder.itemView.rtc_hot1.invalidate()
+
+        holder.itemView.tv_l1r1_adv.text = data[position].products?.get(0)?.advText
+        holder.itemView.tv_l1r2_adv.text = data[position].products?.get(1)?.advText
+
+        holder.itemView.tv_title_l2r1.text = data[position].products?.get(2)?.productTypeName
+        holder.itemView.tv_title_l2r2.text = data[position].products?.get(3)?.productTypeName
+        holder.itemView.tv_title_l2r3.text = data[position].products?.get(4)?.productTypeName
+        holder.itemView.tv_title_l2r4.text = data[position].products?.get(5)?.productTypeName
+
+        holder.itemView.tv_title_adv_l2r1.text = data[position].products?.get(2)?.advText
+        holder.itemView.tv_title_adv_l2r2.text = data[position].products?.get(3)?.advText
+        holder.itemView.tv_title_adv_l2r3.text = data[position].products?.get(4)?.advText
+        holder.itemView.tv_title_adv_l2r4.text = data[position].products?.get(5)?.advText
+
+        Glide.with(context).load(data[position].products?.get(0)?.url?.get(0)).into(holder.itemView.iv_l1r1)
+        Glide.with(context).load(data[position].products?.get(0)?.url?.get(1)).into(holder.itemView.iv_l1r2)
+        Glide.with(context).load(data[position].products?.get(1)?.url?.get(0)).into(holder.itemView.iv_l1r3)
+        Glide.with(context).load(data[position].products?.get(1)?.url?.get(1)).into(holder.itemView.iv_l1r4)
+
+        Glide.with(context).load(data[position].products?.get(2)?.url?.get(0)).into(holder.itemView.iv_l2r1)
+        Glide.with(context).load(data[position].products?.get(3)?.url?.get(0)).into(holder.itemView.iv_l2r2)
+        Glide.with(context).load(data[position].products?.get(4)?.url?.get(0)).into(holder.itemView.iv_l2r3)
+        Glide.with(context).load(data[position].products?.get(5)?.url?.get(0)).into(holder.itemView.iv_l2r4)
+
+        holder.itemView.iv_l1r1.setOnClickListener{
+            RouterManager.getInstance().with(context).action("/AnimUtils/alphaView").callMethod(holder.itemView.iv_l1r1,0.5f,1f,300L)
+            ToastUtils.show("点击了：" + data[position].products?.get(0)?.productTypeName)
+        }
+        holder.itemView.iv_l1r2.setOnClickListener{
+            RouterManager.getInstance().with(context).action("/AnimUtils/alphaView").callMethod(holder.itemView.iv_l1r2,0.5f,1f,300L)
+            ToastUtils.show("点击了：" + data[position].products?.get(0)?.productTypeName)
+        }
+
+        holder.itemView.iv_l1r3.setOnClickListener{
+            RouterManager.getInstance().with(context).action("/AnimUtils/alphaView").callMethod(holder.itemView.iv_l1r3,0.5f,1f,300L)
+            ToastUtils.show("点击了：" + data[position].products?.get(1)?.productTypeName)
+        }
+        holder.itemView.iv_l1r4.setOnClickListener{
+            RouterManager.getInstance().with(context).action("/AnimUtils/alphaView").callMethod(holder.itemView.iv_l1r4,0.5f,1f,300L)
+            ToastUtils.show("点击了：" + data[position].products?.get(1)?.productTypeName)
+        }
+
+        holder.itemView.iv_l2r1.setOnClickListener{
+            RouterManager.getInstance().with(context).action("/AnimUtils/alphaView").callMethod(holder.itemView.iv_l2r1,0.5f,1f,300L)
+            ToastUtils.show("点击了：" + data[position].products?.get(2)?.productTypeName)
+        }
+        holder.itemView.iv_l2r2.setOnClickListener{
+            RouterManager.getInstance().with(context).action("/AnimUtils/alphaView").callMethod(holder.itemView.iv_l2r2,0.5f,1f,300L)
+            ToastUtils.show("点击了：" + data[position].products?.get(3)?.productTypeName)
+        }
+        holder.itemView.iv_l2r3.setOnClickListener{
+            RouterManager.getInstance().with(context).action("/AnimUtils/alphaView").callMethod(holder.itemView.iv_l2r3,0.5f,1f,300L)
+            ToastUtils.show("点击了：" + data[position].products?.get(4)?.productTypeName)
+        }
+        holder.itemView.iv_l2r4.setOnClickListener{
+            RouterManager.getInstance().with(context).action("/AnimUtils/alphaView").callMethod(holder.itemView.iv_l2r4,0.5f,1f,300L)
+            ToastUtils.show("点击了：" + data[position].products?.get(5)?.productTypeName)
+        }
 
     }
 
