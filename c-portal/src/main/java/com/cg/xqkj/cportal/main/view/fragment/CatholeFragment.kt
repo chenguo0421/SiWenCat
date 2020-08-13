2f9064c73f302b5988eb9ec5067b7d53775bf1b7
wushengyuan1hao@163.com <Chenguo19940221@>
2020-08-13 22:54:22 +0800

    【mod】新增猫屋title

--- c-portal/src/main/java/com/cg/xqkj/cportal/main/view/fragment/CatholeFragment.kt
+++ c-portal/src/main/java/com/cg/xqkj/cportal/main/view/fragment/CatholeFragment.kt
@@ -2,11 +2,13 @@ package com.cg.xqkj.cportal.main.view.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import cn.com.cg.base.BaseFragment
 import cn.com.cg.router.annotation.CRouter
 import com.cg.xqkj.cportal.R
 import com.cg.xqkj.cportal.main.contract.CatholeFMContract
 import com.cg.xqkj.cportal.main.presenter.CatholeFMPresenter
+import kotlinx.android.synthetic.main.portal_fragment_gathole.*
 
 /**
  *  author : ChenGuo
@@ -14,7 +16,8 @@ import com.cg.xqkj.cportal.main.presenter.CatholeFMPresenter
  *  description : { 猫屋页面 }
  */
 @CRouter(path = "CatholeFragment")
-class CatholeFragment :CatholeFMContract.IView, BaseFragment<CatholeFMContract.IView, CatholeFMContract.IPresenter<CatholeFMContract.IView>>() {
+class CatholeFragment :CatholeFMContract.IView, BaseFragment<CatholeFMContract.IView, CatholeFMContract.IPresenter<CatholeFMContract.IView>>(),
+    View.OnClickListener {
     private lateinit var bundle: Bundle
     private lateinit var mPresenter: CatholeFMContract.IPresenter<CatholeFMContract.IView>
 
@@ -49,6 +52,25 @@ class CatholeFragment :CatholeFMContract.IView, BaseFragment<CatholeFMContract.I
     }
 
     override fun initData() {
+        iv_collect.setOnClickListener(this)
+        iv_original.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.iv_collect -> {
+                iv_collect.setBackgroundResource(R.drawable.common_circle_half_left_red)
+                iv_collect.setTextColor(getBaseActivity().getColor(R.color.common_white))
+                iv_original.setBackgroundResource(R.drawable.common_circle_half_border_right_red)
+                iv_original.setTextColor(getBaseActivity().getColor(R.color.common_red))
+            }
+            R.id.iv_original -> {
+                iv_collect.setBackgroundResource(R.drawable.common_circle_half_border_left_red)
+                iv_collect.setTextColor(getBaseActivity().getColor(R.color.common_red))
+                iv_original.setBackgroundResource(R.drawable.common_circle_half_right_red)
+                iv_original.setTextColor(getBaseActivity().getColor(R.color.common_white))
+            }
+        }
     }
 
 }
